8e877ca15027a059442cfda21e44bdb85c9fcdd3
Vinayak Bosamiya <vinaybosamiya294@gmail.com>
2025-11-14 16:30:33 +0530

    Add files via upload

---
+++ Hybrid.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+// Level 1
+class A {
+public:
+    void showA() {
+        cout << "I am A" << endl;
+    }
+};
+
+// Level 2 (A -> B)
+class B : public A {
+public:
+    void showB() {
+        cout << "I am B" << endl;
+    }
+};
+
+// Independent class
+class C {
+public:
+    void showC() {
+        cout << "I am C" << endl;
+    }
+};
+
+// Hybrid class (B + C)
+class D : public B, public C {
+public:
+    void showD() {
+        cout << "I am D" << endl;
+    }
+};
+
+int main() {
+    D obj;
+
+    obj.showA();  // from A
+    obj.showB();  // from B
+    obj.showC();  // from C
+    obj.showD();  // from D
+
+    return 0;
+}
