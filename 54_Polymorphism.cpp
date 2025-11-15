8bbcd030dc9bf6a55e2997c2db00aac33a110887
Vinayak Bosamiya <vinaybosamiya294@gmail.com>
2025-11-15 18:08:58 +0530

    Update 54_Polymorphism.cpp

--- 54_Polymorphism.cpp
+++ 54_Polymorphism.cpp
@@ -5,9 +5,15 @@ int main(){
     /* 
     -> polymorphism -> poly means more to more (ghana badha),morphism means form means formate
     -> polymorphism means one name and multiple formate
+    
     -> available 2 types of polymorphism
-            1.compile polymorphism
-            2.runtime polymorphism
+         1.compile polymorphism :-
+               1.1 - function overloading
+               1.2 - operator overloading
+         2.runtime polymorphism :-
+               2.1 - Virtual Functions
+
     -> binding means which obj will call which function 
     */
+
 }
